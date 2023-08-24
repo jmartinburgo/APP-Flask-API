9813347827d5a2c9993ee7e6225acf6eda34fb69
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2023-08-24 09:10:59 -0300

    Last commit

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import requests
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    if request.method=='GET':
+        return render_template('index.html')
+    
+    if request.form['search']:
+        url="https://api.giphy.com/v1/gifs/search?api_key=sE2XWEXDKWPy6okW6DGVDTVe1D1r1s2b&limit=24&q="+request.form['search']
+        giphy= requests.get(url)
+        dataGiphy= giphy.json()
+    
+        return render_template('index.html',data=dataGiphy['data'])
+    else:
+        return render_template('index.html')
+       
 
 if __name__=='__main__':
     app.run(debug=True, port=4000)
